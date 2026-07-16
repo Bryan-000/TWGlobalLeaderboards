a6a6c47a8aca129e0ae976b79572d10cc42a3544
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-07-16 18:53:37 +0100

    Add project files.

---
+++ src/Plugin.cs
@@ -0,0 +1,16 @@
+﻿namespace TWGlobalLeaderboards;
+
+using BepInEx;
+using HarmonyLib;
+
+/// <summary> General plugin handler. </summary>
+[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+public class Plugin : BaseUnityPlugin
+{
+    /// <summary> Load the mod. </summary>
+    public void Awake()
+    {
+        // loading code
+        new Harmony(PluginInfo.GUID).PatchAll();
+    }
+}
---
+++ src/PluginInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace TWGlobalLeaderboards;
+
+/// <summary> Information about the mod. </summary>
+public class PluginInfo
+{
+    /// <summary> 
+    /// The unique identifier for this mod. It shouldn't change between mod versions. 
+    /// <para> It shouldn't change between mod versions. </para>
+    /// </summary>
+    public const string GUID = "Bryan_-000-.TWGlobalLeaderboards";
+
+    /// <summary> 
+    /// The user friendly name of the plugin. 
+    /// <para> Is able to be changed between versions. </para>
+    /// </summary>
+    public const string Name = "TWGlobalLeaderboards";
+
+    /// <summary> The user friendly description of the plugin. </summary>
+    public const string Description = "Description";
+
+    /// <summary> The current version of the plugin. </summary>
+    public const string Version = "1.0.0";
+}
