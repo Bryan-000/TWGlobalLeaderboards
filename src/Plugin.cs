8ff2b08391886d2b141cc34af7a9b1a070be9b82
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-07-18 00:15:52 +0100

    ui

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,15 +1,20 @@
 ﻿namespace TWGlobalLeaderboards;
 
 using BepInEx;
+using HarmonyLib;
 
 [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
 public class Plugin : BaseUnityPlugin
 {
     public void Awake()
     {
+        ModsConfigHelper.EnableModdedSettings();
+        ModsConfigHelper.DestroyDummyButtons();
+
+        new Harmony(PluginInfo.GUID).PatchAll();
     }
 
-    public class PluginInfo
+    public static class PluginInfo
     {
         public const string GUID = "Bryan_-000-.TWGlobalLeaderboards";
         public const string Name = "TWGlobalLeaderboards";
---
+++ src/UIPatches.cs
@@ -0,0 +1,44 @@
+﻿namespace TWGlobalLeaderboards;
+
+using System;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[HarmonyPatch]
+public static class UIPatches
+{
+    [HarmonyPostfix] [HarmonyPatch(typeof(ModsConfigHelper), "Start")]
+    public static void CreateButtons(ModsConfigHelper __instance)
+    {
+        __instance.CreateTitle("Leaderboard Mode:");
+        __instance.CreateSlider();
+    }
+
+    // DUVIZZ FUCKING G LOCCK IINNNNN
+    extension(ModsConfigHelper help)
+    {
+        public ButtonController CreateButton(string text = "Example button", string toolTip = "", Action OnClick = null)
+        {
+            (GameObject obj, UnityEvent onClick) = help.CreateButton(text, toolTip);
+            if (OnClick != null)
+                onClick.AddListener(() => OnClick());
+
+            return obj.GetComponentInChildren<ButtonController>();
+        }
+
+        public Slider CreateSlider(float min = 0f, float max = 100f, float startingValue = 0f)
+        {
+            (GameObject obj, Slider slider) = help.CreateSlider();
+            slider.value = startingValue;
+            slider.minValue = min;
+            slider.maxValue = max;
+
+            return slider;
+        }
+
+        public GameObject CreateTitle(string text = "Example Title") =>
+            help.CreateButton(text); // for some reason this is called createbutton even tho its meant to be create title
+    }
+}
